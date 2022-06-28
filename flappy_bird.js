403cf7262001c3bf3bf33be6c9db4485ac634d1c
fned-ship <72948810+fned-ship@users.noreply.github.com>
2022-06-28 12:29:15 +0200

    Update flappy_bird.js

--- flappy_bird.js
+++ flappy_bird.js
@@ -16,7 +16,7 @@ interstellar.src = "https://www.cjoint.com/doc/19_05/IEdtsrkOjUA_interstellar-ma
 
 var  windowWidth=window.screen.width ;//* window.devicePixelRatio ;
 var  windowHeight=window.screen.height ;//* window.devicePixelRatio ;
-var  user="computer"
+var  user="other"
 var orientLL=false;
 
 var blockW,blockH,gameW,gameH,holeW,holeH,characterW,characterH;
@@ -151,7 +151,8 @@ if ( /android|webOS|iPhone|iPad|iPod|blackberry|IEMobile|Opera Mini|Windows Phon
     }
     onload = new Orientation();
 }else{ //computer
-    document.documentElement.style.setProperty('--gx-height', '45px');
+    user="computer"
+    //document.documentElement.style.setProperty('--gx-height', '45px');
     document.documentElement.style.setProperty('--gx-pdg', '50px');
     document.documentElement.style.setProperty('--gx-font', '30px');
 
@@ -184,7 +185,141 @@ if ( /android|webOS|iPhone|iPad|iPod|blackberry|IEMobile|Opera Mini|Windows Phon
     winningMessageElement.style.width=`${gameW}px`
     winningMessageElement.style.height=`${gameH}px`
 }
+//-----------------------------------------------  other --------------------------------------------------------------------
+var cntr=0
+var vrb=true
+function Orientation(){
+    cntr+=1
+    if(window.screen.width<window.screen.height && cntr==1 ){
+        vrb=true
+    }else if( window.screen.width>=window.screen.height && cntr==1 ){
+        vrb=false
+    }
+    if( window.screen.width<window.screen.height  && vrb  ){//portrait       
+        alert("portrait")
+        vrb=false
+        orientLL=false;
+                if(window.screen.width>window.screen.height){
+                    document.documentElement.style.setProperty('--block-R', `${window.screen.width}px`);
+                    document.documentElement.style.setProperty('--block-L', `${-window.screen.width*0.05}px`);
+                    game.style.width=`${window.screen.width}px`
+                    gameW=window.screen.width
+                    game.style.height=`${window.screen.height}px`
+                    gameH=window.screen.height
+                    //block.style.height=`${window.screen.height}px`
+                    //blockH=window.screen.height
+                    block1.style.width=`${window.screen.width*0.05}px`
+                    block.style.width=`${window.screen.width*0.05}px`
+                    blockW=window.screen.width*0.05
+                    hole.style.width=`${window.screen.width*0.05}px`
+                    holeW=window.screen.width*0.05
+                    hole.style.height=`${window.screen.height*0.5}px`//0.4
+                    holeH=window.screen.height*0.5//0.25
+                    character.style.width=`${window.screen.width*0.1}px`//0.5
+                    characterW=window.screen.width*0.1
+                    character.style.height=`${window.screen.width*0.1}px`//0.5
+                    characterH=window.screen.width*0.1
+                    character.style.top=`${window.screen.height/2-characterH/2}px`
+                    character.style.left=`${window.screen.width/2-characterW/2}px`
+                    block.style.height=`${gameH/2-holeH/2}px`
+                    block1.style.height=`${gameH/2-holeH/2}px`
+                    block1.style.top=`${gameH/2+holeH/2}px`
+                    hole.style.top=`${gameH/2-holeH/2}px`
+                    winningMessageElement.style.width=`${gameW}px`
+                    winningMessageElement.style.height=`${gameH}px`
+                }else{
+                game.style.width=`${window.screen.width}px`
+                gameW=window.screen.width
+                game.style.height=`${window.screen.height}px`
+                gameH=window.screen.height
+                //block.style.height=`${window.screen.height}px`
+                //blockH=window.screen.height
+                hole.style.height=`${window.screen.height*0.5}px`//0.4
+                holeH=window.screen.height*0.5//0.25
+                character.style.width=`${window.screen.width*0.2}px`//0.05
+                characterW=window.screen.width*0.2//0.05
+                character.style.height=`${window.screen.width*0.2}px`
+                characterH=window.screen.width*0.2
+                character.style.top=`${window.screen.height/2-characterH/2}px`
+                character.style.left=`${window.screen.width/2-characterW/2}px`
+                document.documentElement.style.setProperty('--block-R', `${window.screen.width}px`);
+                block.style.height=`${gameH/2-holeH/2}px`
+                block1.style.height=`${gameH/2-holeH/2}px`
+                block1.style.top=`${gameH/2+holeH/2}px`
+                hole.style.top=`${gameH/2-holeH/2}px`
+                if(window.screen.width>=600){
+                    block.style.width=`${window.screen.width*0.08}px`
+                    blockW=window.screen.width*0.08
+                    block1.style.width=`${window.screen.width*0.08}px`
+                    hole.style.width=`${window.screen.width*0.08}px`
+                    holeW=window.screen.width*0.08
+                    document.documentElement.style.setProperty('--block-L', `${-window.screen.width*0.08}px`);
+                }else{
+                    block.style.width=`${window.screen.width*0.1}px`
+                    blockW=window.screen.width*0.1
+                    block1.style.width=`${window.screen.width*0.1}px`
+                    hole.style.width=`${window.screen.width*0.1}px`
+                    holeW=window.screen.width*0.1
+                    document.documentElement.style.setProperty('--block-L', `${-window.screen.width*0.1}px`);
+                }
+                winningMessageElement.style.width=`${gameW}px`
+                winningMessageElement.style.height=`${gameH}px`
+                }
+    }else if( window.screen.width>=window.screen.height  && vrb==false  ){//landscape  
+        alert("landscape")
+        vrb=true
+        orientLL=true;
+                //if(window.screen.width>=600){
+                    document.documentElement.style.setProperty('--block-R', `${window.screen.width}px`);
+                    document.documentElement.style.setProperty('--block-L', `${-window.screen.width*0.05}px`);
+                    game.style.width=`${window.screen.width}px`
+                    gameW=window.screen.width
+                    game.style.height=`${window.screen.height}px`
+                    gameH=window.screen.height
+                    //block.style.height=`${window.screen.height}px`
+                    //blockH=window.screen.height
+                    block1.style.width=`${window.screen.width*0.05}px`
+                    block.style.width=`${window.screen.width*0.05}px`
+                    blockW=window.screen.width*0.05
+                    hole.style.width=`${window.screen.width*0.05}px`
+                    holeW=window.screen.width*0.05
+                    hole.style.height=`${window.screen.height*0.5}px`//0.25
+                    holeH=window.screen.height*0.5//0.25//0.4
+                    character.style.width=`${window.screen.width*0.1}px`//0.5
+                    characterW=window.screen.width*0.1
+                    character.style.height=`${window.screen.width*0.1}px`//0.5
+                    characterH=window.screen.width*0.1
+                    character.style.top=`${window.screen.height/2-characterH/2}px`
+                    character.style.left=`${window.screen.width/2-characterW/2}px`
+                    block.style.height=`${gameH/2-holeH/2}px`
+                    block1.style.height=`${gameH/2-holeH/2}px`
+                    block1.style.top=`${gameH/2+holeH/2}px`
+                    hole.style.top=`${gameH/2-holeH/2}px`
+                    winningMessageElement.style.width=`${gameW}px`
+                    winningMessageElement.style.height=`${gameH}px`
+    }
+}
+function loopOrientation(){
+    
+    Orientation();
 
+    requestAnimationFrame(loopOrientation);
+}
+if(user==="other"  ){// iphone and phones 
+    if( window.screen.width<600 ){
+        bar.style.width="80%"
+    }else{
+        bar.style.width="350px"
+    }
+
+    if(window.screen.width<=300){
+        document.documentElement.style.setProperty('--gx-height', '30px');
+        document.documentElement.style.setProperty('--gx-pdg', '40px');
+        document.documentElement.style.setProperty('--gx-font', '18px');
+    }
+    bar.style.backgroundColor="#00000073"
+    loopOrientation();
+}
 //------------------------------------------------  galaxy fold -------------------------------------------------------------
 function check(){
     if( window.screen.width<600 ){
@@ -364,9 +499,14 @@ setInterval(function(){
     barr.innerText=`Score : ${counter}`
 },10);
 
+var ccctr=0
 function jump(){
-    if(user=="phone"){
+    if(user=="phone" || user=="other" ){
         document.documentElement.requestFullscreen();
+    }
+    ccctr+=1
+    if(ccctr<=3){
+        alert("click");
     }
     jumping = 1;
     let jumpCount = 0;
@@ -374,6 +514,9 @@ function jump(){
         var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         if((characterTop>6) &&(jumpCount<19) && canJump ){//&&(jumpCount<15)//19
             character.style.top = (characterTop-5)+"px";//5
+            if(ccctr<=3  && jumpCount<=2 ){
+                alert("characterTop"+characterTop+" => "+(characterTop-5));
+            }
             //let imgSrc = imgBird.getAttribute("src");
             //let BIRD_IMG = imgSrc == "bird-up.png" ? "bird-down.png" : "bird-up.png";
             //imgBird.setAttribute("src", BIRD_IMG);
